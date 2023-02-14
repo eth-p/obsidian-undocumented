b3f165bc14c33c6c292b3966350f17f5b7e68b55
Ethan P <eth-p+git@hidden.email>
2023-02-14 14:35:48 -0800

    fix(app): Add setting property

--- src/typings/app.d.ts
+++ src/typings/app.d.ts
@@ -8,16 +8,18 @@ import { WorkspaceUndocumented } from "./workspace";
 import { PluginManagerUndocumented } from "./plugin-manager";
 import { StyleManagerUndocumented } from "./style-manager";
 import { ViewRegistryUndocumented } from "./view-registry";
+import { SettingManagerUndocumented } from "./setting-manager";
 
 /**
  * The Obsidian {@link App} object, with undocumented and private APIs included.
  */
 export type AppUndocumented<V extends Version = Latest, U extends MaybeUnsafe = false> = App &
-    ForVersion<v1_0_0, V, Latest, {
+	ForVersion<v1_0_0, V, Latest, {
 
-        readonly workspace: WorkspaceUndocumented<V, U>;
+		readonly workspace: WorkspaceUndocumented<V, U>;
 		readonly plugins: PluginManagerUndocumented<V, U>;
 		readonly viewRegistry: ViewRegistryUndocumented<V, U>;
 		readonly customCss: StyleManagerUndocumented<V, U>;
+		readonly setting: SettingManagerUndocumented<V, U>;
 
-    }>;
+	}>;
